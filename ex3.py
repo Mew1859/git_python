6fc8e99e74c3454d311fa1201f40e8ff1a775eff
Mew1859 <68700706+Mew1859@users.noreply.github.com>
2020-07-24 14:48:17 +0700

    part1
    part1 24/7/63

---
+++ ex3.py
@@ -0,0 +1,12 @@
+#Data Type & Variable
+#ชื่อตัวแปร = ค่าที่เก็บในตัวแปร
+#ชนิดข้อมูล ฃื่อตัวแปร = ค่าที่เก็บตัวแปร
+x = 10
+y = 3.99
+z = True
+print ("ผลลัพธ์ = " +str(x))
+print(y)
+print(z)
+print(type(y))
+print(type("python"))
+print(type(z))
